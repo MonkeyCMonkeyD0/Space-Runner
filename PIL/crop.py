ba0776a560e59fcb82957eff9ad8e4744171dbe7
QuartoTR <tristan.michel@etu.umpc.fr>
2021-03-23 10:34:49 +0100

    Updating .gitignore, PIL, icon

--- PIL/crop.py
+++ PIL/crop.py
@@ -6,15 +6,20 @@ import sys
 nameorig=sys.argv[1]
 print (nameorig)
 
+lx=[15,149,285,421,556,698,829,964]
+ly=[3,138,274,410]
 im_orig=Image.open(nameorig)
 w,h = im_orig.size
 print (w,h)
-for y in range(8):
-	for x in range(14):
-		box=(x*123,y*123,(x+1)*123,(y+1)*123)
+for y in range(4):
+	for x in range(8):
+		# 15, 149, 285, 421, 556
+		#box=(x*135+15,y*135+3,x*135+15+122,y*135+3+122)
+		box=(lx[x],ly[y],lx[x]+120,ly[y]+120)
 		im_crop=im_orig.crop(box)
+		im_resize=im_crop.resize((256,256))
 
 		namesave="ressources/res4_%d%d.png"%(y,x)
 		print (namesave)
-		im_crop.save(namesave,"PNG")
+		im_resize.save(namesave,"PNG")
 
--- PIL/text.py
+++ PIL/text.py
@@ -1,12 +1,13 @@
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 
-source_img = Image.new(mode = 'RGB', size = (256,256 ), color = (0,0,0))
+source_img = Image.new(mode = 'RGB', size = (256,256 ), color = (20,20,128))
 #source_img = Image.open("sci-fi-icon-11.png").convert("RGBA")
 
 draw = ImageDraw.Draw(source_img)
-font = ImageFont.truetype("bebas.ttf", 50)
-#draw.rectangle(((0, 00), (100, 100)), fill="black")
-draw.text((20, 70), "MAIN4", font=font)
+font = ImageFont.truetype("bebas.ttf", 150)
+w,h= draw.textsize("$503", font=font)
+print (w,h)
+draw.text((128-w/2, 128-h/2), "$503", font=font)
 #draw.text((20, 70), "something123")
 
 source_img.save("test.png", "png")
